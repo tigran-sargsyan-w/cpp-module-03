695e5c24e529efa753047b5795284aa55960b99c
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2025-12-18 12:38:46 +0100

    add: copy from ex00 class clapTrap

---
+++ ex01/main.cpp
@@ -0,0 +1,32 @@
+#include "ClapTrap.hpp"
+
+/**
+ * @brief Main function.
+ * Tests the functionality of the ClapTrap class.
+ * @return 0 on success.
+ */
+int	main(void)
+{
+	ClapTrap a("A");
+	ClapTrap b("B");
+
+	a.attack("dummy");
+	b.takeDamage(3);
+	b.beRepaired(2);
+
+	b.takeDamage(100);
+	b.attack("someone");
+	b.beRepaired(5);
+
+	std::cout << "\n--- Energy drain test ---\n";
+	ClapTrap c("C");
+	for (int i = 0; i < 11; ++i)
+		c.attack("wall");
+
+	std::cout << "\n--- Copy test ---\n";
+	ClapTrap d(c);
+	ClapTrap e("E");
+	e = a;
+
+	return (0);
+}
